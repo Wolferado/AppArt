9ec62fd634536160da61a0490bf4e70c138ee854
Aleksejs Kareļins <akarelins125@gmail.com>
2022-04-02 10:38:18 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+const events = [];
+
+var eventContainer = document.getElementById("event-container");
+
+(function populateEvents() {
+    if(events.length == 0) {
+        let notification = document.createElement("h3");
+        notification.textContent = "There are no new events available.";
+        eventContainer.appendChild(notification);
+    }
+    else {
+        
+    }
+})();
