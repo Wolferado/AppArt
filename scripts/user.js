a58cecc95ff2fdec1c8837e5b1a421fee14da680
Aleksejs Kareļins <akarelins125@gmail.com>
2022-04-03 12:52:41 +0300

    Responsive Design Created

--- scripts/user.js
+++ scripts/user.js
@@ -1,10 +1,24 @@
-//Script dedicated to check, if user logged in.
+//Script dedicated to check, if user logged in and open menu on mobile.
 
 (function checkIfUserLoggedIn() {
     if(sessionStorage.getItem("username") != null) {
-        let profileLink = document.getElementsByTagName("a")[4];
+        let profileLink = document.getElementsByTagName("a")[5];
         profileLink.style.border = "none";
         profileLink.href = "profile_settings.html";
         profileLink.textContent = sessionStorage.getItem("username") + " - profile";
     }
 })();
+
+var menuSwitch = document.getElementById("switch");
+menuSwitch.addEventListener('click', openMenu);
+
+function openMenu() {
+    let navigation = document.getElementsByTagName("nav")[0];
+    
+    if(navigation.style.display == "none") {
+        navigation.style.display = "flex";
+    }
+    else {
+        navigation.style.display = "none";
+    }
+}
